d4b5f3bf1231b4d760be4ce7d47623fb2c33e976
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-15 23:10:48 +0100

    Only accepting PDF contracts

--- components/ContractUploader.tsx
+++ components/ContractUploader.tsx
@@ -58,6 +58,7 @@ export default function ContractUploader() {
 				<CardContent>
 					<input
 						type='file'
+                        accept='application/pdf'
 						onChange={handleFileChange}
 						className='block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 mb-4'
 					/>
