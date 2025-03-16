fe2bd86542a1293a596e70f548c6dccf00fb2462
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-16 15:35:00 +0100

    Added link to landingpage to the logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,12 +24,14 @@ export default function Header() {
 	return (
 		<header className='sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
 			<div className='container flex h-16 items-center justify-between'>
-				<div className='flex items-center gap-1'>
-					<Shield className='h-6 w-6 text-accent' />
-					<span className='text-xl font-semibold tracking-tight'>
-						Legal<span className='text-primary'>Edge</span>
-					</span>
-				</div>
+				<Link href='/'>
+					<div className='flex items-center gap-1'>
+						<Shield className='h-6 w-6 text-accent' />
+						<span className='text-xl font-semibold tracking-tight'>
+							Legal<span className='text-primary'>Edge</span>
+						</span>
+					</div>
+				</Link>
 				<nav className='hidden md:flex gap-6 text-md'>
 					{navItems.map((item) => (
 						<Link
