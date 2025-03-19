c286a2fb986cb78011610bc21dbac9313b4785c7
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-19 13:50:54 +0100

    File for adding appwrite admin client for doing db queries

---
+++ appwrite/config.ts
@@ -0,0 +1,68 @@
+import { Client, Databases, Account, Storage } from 'node-appwrite';
+
+const createAdminClient = async () => {
+	console.log(
+		`Creating admin client with endpoint: ${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}, project ID: ${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`,
+	);
+
+	const client = new Client()
+		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '')
+		.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '')
+		.setKey(process.env.APPWRITE_SECRET_KEY || '');
+
+	return {
+		get account() {
+			return new Account(client);
+		},
+		get databases() {
+			return new Databases(client);
+		},
+		get storage() {
+			return new Storage(client);
+		},
+	};
+};
+
+const createSessionClient = async (session: any) => {
+	const client = new Client()
+		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '')
+		.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '');
+
+	if (session) {
+		client.setSession(session);
+	}
+
+	return {
+		get account() {
+			return new Account(client);
+		},
+		get databases() {
+			return new Databases(client);
+		},
+		get storage() {
+			return new Storage(client);
+		},
+		get client() {
+			return client;
+		},
+	};
+};
+
+// Shouldnt need this anymore, as we added get storage() to createSessionClient and createAdminClient
+const createStorageClient = async (session: any) => {
+	const client = new Client()
+		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '')
+		.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '');
+
+	if (session) {
+		client.setSession(session);
+	}
+
+	return {
+		get storage() {
+			return new Storage(client);
+		},
+	};
+};
+
+export { createAdminClient, createSessionClient, createStorageClient };
