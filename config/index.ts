a65c3dc383946d11a30e9d8e663d1980b78068fa
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-19 13:52:23 +0100

    Config constants for token prices and stripe variables

---
+++ config/index.ts
@@ -0,0 +1,14 @@
+// STRIPE CONFIG CONSTANTS
+export const CURRENCY = 'nok';
+// Set your amount limits: Use float for decimal currencies and
+// Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
+export const MIN_AMOUNT = 10.0;
+export const MAX_AMOUNT = 500.0;
+export const AMOUNT_STEP = 2.0;
+// END STRIPE CONFIG
+
+// GENERAL CONFIG
+export const TOKENS_PER_QUERY = 2;
+export const TOKENS_PER_PREMIUM_QUERY = 6;
+export const START_TOKENS = 2;
+
