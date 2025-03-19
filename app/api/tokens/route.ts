6d91cee52f868e9b6d3b12ff3f6795055aa1fb3b
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-19 13:49:39 +0100

    Route handler for getting tokens, and revalidating after mutation

---
+++ app/api/tokens/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+import { createAdminClient } from "@/appwrite/config";
+import { auth } from "@clerk/nextjs/server";
+import { setUpTokensForFirstTimeUser } from "@/app/actions/tokens";
+import { START_TOKENS } from "@/config";
+
+// This is for fetching the users document_quota_left from the appwrite database. It needs to validate the user first.
+export async function GET() {
+    const { userId } = await auth();
+    if (!userId) {
+        return NextResponse.json({ error: "User ID is missing" }, { status: 400 });
+    }
+
+    const { databases } = await createAdminClient();
+    const result = await databases.getDocument('legaledge', 'user_queries', userId);
+    // if no document is found, the user is new and we need to set up the tokens for them (this should never happen, as the new user tokens should be set up by clerk webhooks)
+    if (!result.$id) {
+        setUpTokensForFirstTimeUser(userId);
+        return NextResponse.json({ tokens: START_TOKENS });
+    }
+    return NextResponse.json({ tokens: result.document_quota_left });
+}
