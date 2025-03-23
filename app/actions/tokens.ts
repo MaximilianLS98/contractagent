a7b0418a0e005588d6376e018c59e9001a8a9cb6
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-23 22:23:32 +0100

    getting appwrite variables from .env

--- app/actions/tokens.ts
+++ app/actions/tokens.ts
@@ -3,21 +3,23 @@ import { Query, ID } from "node-appwrite";
 import { START_TOKENS, TOKENS_PER_QUERY } from "@/config";
 import { revalidateTag } from "next/cache";
 
+const dbId = process.env.APPWRITE_DB_ID as string;
+
 export async function addTokens(clerk_user_id: string, numTokens: number) {
     const { databases } = await createAdminClient();
-    const documentToUpdate = await databases.listDocuments('legaledge', 'user_queries', [
+    const documentToUpdate = await databases.listDocuments(dbId, 'user_queries', [
         Query.select(['$id', 'document_quota_left']),
         Query.equal('clerk_user_id', clerk_user_id)
     ]);
     if (documentToUpdate.documents.length === 0) {
-        await databases.createDocument('legaledge', 'user_queries', clerk_user_id, {
+        await databases.createDocument(dbId, 'user_queries', clerk_user_id, {
             clerk_user_id: clerk_user_id,
             document_quota_left: numTokens
         });
     } else {
         const docId = documentToUpdate.documents[0].$id;
         const tokens = documentToUpdate.documents[0].document_quota_left + numTokens;
-        await databases.updateDocument('legaledge', 'user_queries', docId, {
+        await databases.updateDocument(dbId, 'user_queries', docId, {
             document_quota_left: tokens
         });
     }
@@ -26,7 +28,7 @@ export async function addTokens(clerk_user_id: string, numTokens: number) {
 
 export async function setUpTokensForFirstTimeUser(clerk_user_id: string) {
     const { databases } = await createAdminClient();
-    await databases.createDocument('legaledge', 'user_queries', clerk_user_id, {
+    await databases.createDocument(dbId, 'user_queries', clerk_user_id, {
         clerk_user_id: clerk_user_id,
         document_quota_left: START_TOKENS
     });
