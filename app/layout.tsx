7993710475980d25f3faada0439e3f4e99cfe2d2
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-25 20:51:20 +0100

    Componetized Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 import { DemoBanner } from "@/components/Demo-banner"
 import { ClerkProvider } from '@clerk/nextjs';
 import { currentUser } from "@clerk/nextjs/server"
@@ -46,6 +47,7 @@ export default async function RootLayout({
 					<Header userTokens={data.tokens} />
 					<DemoBanner />
 					{children}
+          <Footer />
           </PostHogProvider>
 				</body>
 			</html>
