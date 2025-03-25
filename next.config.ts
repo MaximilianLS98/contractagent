445646e285679dd553ff97dfb94a0cd4de16b3c1
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-25 20:50:48 +0100

    Upping the limit for server actions body to 5mb for pdf upload

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://eu-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://eu.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://eu.i.posthog.com/decide",
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
-  /* config options here */
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '5mb',
+		},
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/ingest/static/:path*',
+				destination: 'https://eu-assets.i.posthog.com/static/:path*',
+			},
+			{
+				source: '/ingest/:path*',
+				destination: 'https://eu.i.posthog.com/:path*',
+			},
+			{
+				source: '/ingest/decide',
+				destination: 'https://eu.i.posthog.com/decide',
+			},
+		];
+	},
+	skipTrailingSlashRedirect: true,
+	/* config options here */
 };
 
 export default nextConfig;
