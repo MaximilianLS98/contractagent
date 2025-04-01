146b546c9b06c588d67bce63ae4f81cdf4220ab7
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-04-01 11:58:05 +0200

    Added server action for getting tokens

--- app/actions/tokens.ts
+++ app/actions/tokens.ts
@@ -6,6 +6,18 @@ import { revalidateTag } from "next/cache";
 const dbId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string;
 const collectionId = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID as string;
 
+export async function getTokens(clerk_user_id: string) {
+    const { databases } = await createAdminClient();
+    const result = await databases.listDocuments(dbId, 'user_queries', [
+        Query.select(['$id', 'document_quota_left']),
+        Query.equal('clerk_user_id', clerk_user_id)
+    ]);
+    if (result.documents.length === 0) {
+        return START_TOKENS;
+    }
+    return result.documents[0].document_quota_left;
+}
+
 export async function addTokens(clerk_user_id: string, numTokens: number) {
     const { databases } = await createAdminClient();
     const documentToUpdate = await databases.listDocuments(dbId, 'user_queries', [
