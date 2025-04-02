745721c53bab31cfd1eb626143a2c55c5b1806fa
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-04-02 15:31:58 +0200

    Added buytokens to protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/livedemo']);
+const isProtectedRoute = createRouteMatcher(['/livedemo', '/buytokens']);
 
 export default clerkMiddleware(async (auth, req) => {
 	const { userId, redirectToSignIn } = await auth();
